3ff0fee532d66ad27c050bb32c06f479c3ba8eae
“Arun20” <queenprince20@gmail.com>
2022-04-22 09:11:30 +0000

    3rd commit

--- samplegit-1.py
+++ samplegit-1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 DF = pd.read_csv("https://raw.githubusercontent.com / fivethirtyeight / data / master / airline-safety / airline-safety.csv")
-x = list(DF.aorline-safety)
+x = list(DF.aorline-safety-management)
 plt.boxplot(x)
 plt.show()
 
